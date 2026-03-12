3fff1e88739cf50c17df1b1e2f934b1a7c4ebd88
BigC17 <christophermalecki@gmail.com>
2026-03-12 11:17:02 -0700

    blah

--- BattleScreen.py
+++ BattleScreen.py
@@ -13,19 +13,33 @@ class BattleScreen(Screen):
         ]
         
     def elementsToDisplay(self):
-        self.elements = []
+        self.elements = [
+            Image((50 , 50), 100, 100, './imgs/battlescreen.jpg')
+        ]
 
-        y = 0
-        #two rows of three
-        for trainer in self.trainers:
-            x = 0
-            y += 100/3
-            for poke in trainer.pokemon:
-                x += 100/4
-                self.elements.append(Image((x, y), 20, 20, poke.img))
-                self.elements.append(Label((x, y + 10), 20, 10, poke.name))
+        # y = 0
+        # #two rows of three
+        # for trainer in self.trainers:
+        #     x = 0
+        #     y += 100/3
+        #     for poke in trainer.pokemon:
+        #         x += 100/4
+        #         self.elements.append(Image((x, y), 20, 20, poke.img))
+        #         self.elements.append(Label((x, y + 10), 20, 10, poke.name))
                 
+ #place closest active pokmeon
+        p1Active = self.trainers[0].pokemon[0]
+        x = 30
+        y = 25
+        self.elements.append(Image((x,y), 20, 20, p1Active.img))
 
+
+        #place furthest active pokmeon
+        p2Active = self.trainers[1].pokemon[0]
+        x = 70
+        y = 50
+        self.elements.append(Image((x,y), 20, 20, p2Active.img))
+               
 
 
 
